b3b65ffbb599ba9e176ef84de270a167d0ed6e85
edipo <edipo.as@gmail.com>
2026-04-22 01:32:35 -0300

    import SecurityConfig in IT tests

--- src/test/java/com/edipo/ledger/integration/api/TransactionControllerIT.java
+++ src/test/java/com/edipo/ledger/integration/api/TransactionControllerIT.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
